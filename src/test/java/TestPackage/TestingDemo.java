7899fe707b52dbaef8a002ac46d02cfdc982d191
chowdhur <rajdeepdeepbkp@gmail.com>
2016-04-18 22:00:33 +0530

    Changing firefox profile

--- src/test/java/TestPackage/TestingDemo.java
+++ src/test/java/TestPackage/TestingDemo.java
@@ -1,9 +1,12 @@
 package TestPackage;
 
-import org.openqa.selenium.By;
+import java.io.File;
+
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.firefox.internal.ProfilesIni;
 import org.testng.annotations.Test;
 
 public class TestingDemo {
@@ -13,7 +16,10 @@ public class TestingDemo {
 		System.out.println("I am Rajdeep. I am testing selenium jenkins integration...");
 		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\chowdhur\\.m2\\repository\\pl\\oakfusion\\chromedriver\\2.9\\chromedriver-2.9\\windows\\chromedriver.exe");
 		//WebDriver driver = new ChromeDriver();
-		WebDriver driver = new FirefoxDriver();
+		ProfilesIni prof = new ProfilesIni();
+		FirefoxProfile firefoxProfile = prof.getProfile("Rajdeep");
+		      
+		WebDriver driver = new FirefoxDriver(firefoxProfile);
 		driver.get("http://www.facebook.com");
 		/*
 		driver.findElement(By.xpath("//*[@id='email']")).sendKeys("rajdeepdeepbkp@gmail.com");
