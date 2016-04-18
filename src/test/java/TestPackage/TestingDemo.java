916cee33b01d6f84bdd2a1e2f5bde4408a16eacf
chowdhur <rajdeepdeepbkp@gmail.com>
2016-04-18 22:25:36 +0530

    Second

--- src/test/java/TestPackage/TestingDemo.java
+++ src/test/java/TestPackage/TestingDemo.java
@@ -16,17 +16,17 @@ public class TestingDemo {
 		System.out.println("I am Rajdeep. I am testing selenium jenkins integration...");
 		//System.setProperty("webdriver.chrome.driver", "C:\\Users\\chowdhur\\.m2\\repository\\pl\\oakfusion\\chromedriver\\2.9\\chromedriver-2.9\\windows\\chromedriver.exe");
 		//WebDriver driver = new ChromeDriver();
-		ProfilesIni prof = new ProfilesIni();
+		/*ProfilesIni prof = new ProfilesIni();
 		FirefoxProfile firefoxProfile = prof.getProfile("Rajdeep");
 		      
 		WebDriver driver = new FirefoxDriver(firefoxProfile);
 		driver.get("http://www.facebook.com");
-		/*
+		
 		driver.findElement(By.xpath("//*[@id='email']")).sendKeys("rajdeepdeepbkp@gmail.com");
 		driver.findElement(By.xpath("//*[@id='pass']")).sendKeys("i(love)usomuch!!!");
-		driver.findElement(By.xpath("//*[@id='loginbutton']")).click();*/
+		driver.findElement(By.xpath("//*[@id='loginbutton']")).click();
 		
-		System.out.println("Title: " + driver.getTitle());
+		System.out.println("Title: " + driver.getTitle());*/
 	}
 
 }
