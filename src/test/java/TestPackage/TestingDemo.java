1cf6f60f3687f76cfe2b76101fa35bfeff7b50c7
chowdhur <rajdeepdeepbkp@gmail.com>
2016-04-18 19:35:51 +0530

    Fourth Commit

--- src/test/java/TestPackage/TestingDemo.java
+++ src/test/java/TestPackage/TestingDemo.java
@@ -1,5 +1,6 @@
 package TestPackage;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -12,7 +13,12 @@ public class TestingDemo {
 		System.out.println("I am Rajdeep. I am testing selenium jenkins integration...");
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\chowdhur\\.m2\\repository\\pl\\oakfusion\\chromedriver\\2.9\\chromedriver-2.9\\windows\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://www.google.com");
+		driver.get("http://www.facebook.com");
+		
+		driver.findElement(By.xpath("//*[@id='email']")).sendKeys("rajdeepdeepbkp@gmail.com");
+		driver.findElement(By.xpath("//*[@id='pass']")).sendKeys("i(love)usomuch!!!");
+		driver.findElement(By.xpath("//*[@id='loginbutton']")).click();
+		
 		System.out.println("Title: " + driver.getTitle());
 	}
 
