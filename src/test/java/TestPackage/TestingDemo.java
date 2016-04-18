4ca2f73ad7b096b4bc4e1a5a391ec6a6aa0bbbf8
chowdhur <rajdeepdeepbkp@gmail.com>
2016-04-18 18:52:07 +0530

    Second Commit

--- src/test/java/TestPackage/TestingDemo.java
+++ src/test/java/TestPackage/TestingDemo.java
@@ -10,9 +10,9 @@ public class TestingDemo {
 	@Test
 	public void test(){
 		System.out.println("I am Rajdeep. I am testing selenium jenkins integration...");
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\chowdhur\\.m2\\repository\\pl\\oakfusion\\chromedriver\\2.9\\chromedriver-2.9\\windows\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.home") + "\\.m2\\repository\\pl\\oakfusion\\chromedriver\\2.9\\chromedriver-2.9\\windows\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://www.facebook.com");
+		driver.get("http://www.google.com");
 		System.out.println("Title: " + driver.getTitle());
 	}
 
